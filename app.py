b6680b228b6572656bf7496d40248f420aabaa1c
jasminkarki <jasminkarki8@gmail.com>
2021-01-07 23:49:18 +0545

    Create application file

--- app.py
+++ app.py
@@ -1,32 +1,35 @@
-from flask import Flask, Response
-from flask_cors import CORS
-from flask import request, jsonify
-from bson.json_util import dumps
-from flask_pymongo import PyMongo
-from config.config import shared_components
-import numpy as np
-import pickle
-import json
 import ast
+import json
+import pickle
+import random
+
+import numpy as np
+from bson.json_util import dumps
+from emotion_classify.config.config import shared_components
+from flask import Flask, Response, jsonify, request
+from flask_cors import CORS
+from flask_pymongo import PyMongo
+
 from emotion_classify import baseline_model
 
-app = Flask(__name__)  
+app = Flask(__name__)
 
-model = pickle.load(open('./emotion_classify/model_nb.pkl','rb'))
-app.config.from_pyfile('config/config.cfg')
+model = pickle.load(open("./emotion_classify/model_nb.pkl", "rb"))
+app.config.from_pyfile("./emotion_classify/config/config.cfg")
 CORS(app)
 mongo = PyMongo(app)
 
 # Select the database
 db = mongo.db
-shared_components['db'] = db
+shared_components["db"] = db
+
 
 def create_log(params):
     """
     Function to create new logs.
     """
     db = shared_components["db"]
-    collection = db.log   # Collection name
+    collection = db.log  # Collection name
     try:
         # Insert Text
         collection.insert(json.loads(params))
@@ -35,7 +38,8 @@ def create_log(params):
         # Error while trying to insert the text
         print("Exception: {}".format(e))
 
-@app.route('/api/logs',methods=['GET']) 
+
+@app.route("/api/logs", methods=["GET"])
 def fetch_logs():
     """
        Function to fetch the users.
@@ -44,13 +48,13 @@ def fetch_logs():
     collection = db.log
     try:
         # Fetch all the texts
-        logs_fetched = collection.find({},{'_id':0})
+        logs_fetched = collection.find({}, {"_id": 0})
 
         # Check if the texts are found
         if logs_fetched.count() > 0:
             # Prepare the response
             records = dumps(logs_fetched)
-            resp = Response(records, status=200, mimetype='application/json')
+            resp = Response(records, status=200, mimetype="application/json")
             return resp
         else:
             # No records are found
@@ -60,7 +64,8 @@ def fetch_logs():
         # Error while trying to fetch the resource
         return Response("Error while trying to fetch the resource", status=500)
 
-@app.route('/api/log/<log_id>',methods=['GET']) 
+
+@app.route("/api/log/<log_id>", methods=["GET"])
 def fetch_log(log_id):
     """
        Function to fetch the log.
@@ -69,13 +74,13 @@ def fetch_log(log_id):
     collection = db.log
     try:
         # Fetch one the record(s)
-        records_fetched = collection.find_one({"id": log_id},{'_id':0})
+        records_fetched = collection.find_one({"id": log_id}, {"_id": 0})
 
         # Check if the records are found
         if records_fetched:
             # Prepare the response
             records = dumps(records_fetched)
-            resp = Response(records, status=200, mimetype='application/json')
+            resp = Response(records, status=200, mimetype="application/json")
             return resp
         else:
             # No records are found
@@ -85,14 +90,15 @@ def fetch_log(log_id):
         # Error while trying to fetch the resource
         return Response("Error while trying to fetch the resource", status=500)
 
-@app.route('/api/log/<log_id>',methods=['UPDATE']) 
+
+@app.route("/api/log/<log_id>", methods=["UPDATE"])
 def update_log(log_id):
     """
        Function to update the user.
     """
 
     db = shared_components["db"]
-    collection = db.user
+    collection = db.log
     try:
         # Get the value which needs to be updated
         if request.json:
@@ -101,8 +107,7 @@ def update_log(log_id):
             raise TypeError("Invalid request format")
 
         # Updating the user
-        records_updated = collection.update_one(
-            {"id": log_id}, {"$set": body})
+        records_updated = collection.update_one({"id": log_id}, {"$set": body})
 
         # Check if resource is updated
         if records_updated.modified_count > 0:
@@ -118,13 +123,14 @@ def update_log(log_id):
         print("Exception: {}".format(e))
         return Response("Error while updating the resource", status=500)
 
-@app.route('/api/logs/<log_id>',methods=['DELETE']) 
-def remove_user(log_id):
+
+@app.route("/api/log/<log_id>", methods=["DELETE"])
+def remove_log(log_id):
     """
        Function to remove the user.
     """
     db = shared_components["db"]
-    collection = db.user
+    collection = db.log
     try:
         # Delete the user
         delete_user = collection.delete_one({"id": log_id})
@@ -142,23 +148,28 @@ def remove_user(log_id):
         return Response("Resource deletion failed", status=500)
 
 
-@app.route('/')   
+@app.route("/")
 def display_page():
     return "This is the landing page"
     # return render_template("home.html")
 
-@app.route('/api/predict',methods=['POST'])   
+
+@app.route("/api/predict", methods=["POST"])
 def predict():
     text_inp = request.get_data()
-    data= json.loads(text_inp)
+    data = json.loads(text_inp)
     # # Send for prediction
     output = baseline_model.inference(data["data"])
     if output:
-        output=output[0]
-    create_log({"output":output, "text": data["data"]})
+        output = output[0]
+    create_log(
+        json.dumps(
+            {"output": output, "text": data["data"], "id": str(random.getrandbits(8))}
+        )
+    )
     return jsonify({"output": str(output)})
 
 
+if __name__ == "__main__":
+    app.run("0.0.0.0", 3000, debug=True)  # Automatic run of script
 
-if __name__== "__main__":
-    app.run("0.0.0.0", 3000, debug=True)   # Automatic run of script

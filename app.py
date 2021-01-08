ec854c6306dce8f49929595fe877d2a6eb3929a1
jasminkarki <jasminkarki8@gmail.com>
2021-01-08 10:28:54 +0545

    Move functions to services/log.py

--- app.py
+++ app.py
@@ -2,173 +2,56 @@ import ast
 import json
 import pickle
 import random
+import os
 
 import numpy as np
 from bson.json_util import dumps
-from emotion_classify.config.config import shared_components
 from flask import Flask, Response, jsonify, request
 from flask_cors import CORS
 from flask_pymongo import PyMongo
 
-from emotion_classify import baseline_model
-
-app = Flask(__name__)
-
-model = pickle.load(open("./emotion_classify/model_nb.pkl", "rb"))
-app.config.from_pyfile("./emotion_classify/config/config.cfg")
-CORS(app)
-mongo = PyMongo(app)
-
-# Select the database
-db = mongo.db
-shared_components["db"] = db
+from emotion_classify.config.config import shared_components, BASE_DIR
+from services.log import (
+    display_page,
+    fetch_log,
+    fetch_logs,
+    predict,
+    remove_log,
+    update_log,
+)
 
 
-def create_log(params):
-    """
-    Function to create new logs.
-    """
-    db = shared_components["db"]
-    collection = db.log  # Collection name
-    try:
-        # Insert Text
-        collection.insert(json.loads(params))
+def init_app():
+    app = Flask(__name__)
+    app = create_route(app)
 
-    except Exception as e:
-        # Error while trying to insert the text
-        print("Exception: {}".format(e))
-
-
-@app.route("/api/logs", methods=["GET"])
-def fetch_logs():
-    """
-       Function to fetch the users.
-    """
-    db = shared_components["db"]
-    collection = db.log
-    try:
-        # Fetch all the texts
-        logs_fetched = collection.find({}, {"_id": 0})
-
-        # Check if the texts are found
-        if logs_fetched.count() > 0:
-            # Prepare the response
-            records = dumps(logs_fetched)
-            resp = Response(records, status=200, mimetype="application/json")
-            return resp
-        else:
-            # No records are found
-            return Response("No records are found", status=404)
-    except Exception as e:
-        print("Exception: {}".format(e))
-        # Error while trying to fetch the resource
-        return Response("Error while trying to fetch the resource", status=500)
-
-
-@app.route("/api/log/<log_id>", methods=["GET"])
-def fetch_log(log_id):
-    """
-       Function to fetch the log.
-    """
-    db = shared_components["db"]
-    collection = db.log
-    try:
-        # Fetch one the record(s)
-        records_fetched = collection.find_one({"id": log_id}, {"_id": 0})
-
-        # Check if the records are found
-        if records_fetched:
-            # Prepare the response
-            records = dumps(records_fetched)
-            resp = Response(records, status=200, mimetype="application/json")
-            return resp
-        else:
-            # No records are found
-            return Response("No records are found", status=404)
-    except Exception as e:
-        print("Exception: {}".format(e))
-        # Error while trying to fetch the resource
-        return Response("Error while trying to fetch the resource", status=500)
-
-
-@app.route("/api/log/<log_id>", methods=["UPDATE"])
-def update_log(log_id):
-    """
-       Function to update the user.
-    """
-
-    db = shared_components["db"]
-    collection = db.log
-    try:
-        # Get the value which needs to be updated
-        if request.json:
-            body = ast.literal_eval(json.dumps(request.json))
-        else:
-            raise TypeError("Invalid request format")
-
-        # Updating the user
-        records_updated = collection.update_one({"id": log_id}, {"$set": body})
-
-        # Check if resource is updated
-        if records_updated.modified_count > 0:
-            # Prepare the response as resource is updated successfully
-            return Response("Resource updated successfully", status=200)
-        else:
-            # Bad request as the resource is not available to update
-            # Add message for debugging purpose
-            return Response("Resource not available", status=404)
-    except Exception as e:
-        # Error while trying to update the resource
-        # Add message for debugging purpose
-        print("Exception: {}".format(e))
-        return Response("Error while updating the resource", status=500)
-
-
-@app.route("/api/log/<log_id>", methods=["DELETE"])
-def remove_log(log_id):
-    """
-       Function to remove the user.
-    """
-    db = shared_components["db"]
-    collection = db.log
-    try:
-        # Delete the user
-        delete_user = collection.delete_one({"id": log_id})
-
-        if delete_user.deleted_count > 0:
-            # Prepare the response
-            return Response("Resource deleted successfully", status=200)
-        else:
-            # Resource Not found
-            return Response("Resource Not Found", status=404)
-    except Exception as e:
-        # Error while trying to delete the resource
-        # Add message for debugging purpose
-        print("Exception: {}".format(e))
-        return Response("Resource deletion failed", status=500)
-
-
-@app.route("/")
-def display_page():
-    return "This is the landing page"
-    # return render_template("home.html")
-
-
-@app.route("/api/predict", methods=["POST"])
-def predict():
-    text_inp = request.get_data()
-    data = json.loads(text_inp)
-    # # Send for prediction
-    output = baseline_model.inference(data["data"])
-    if output:
-        output = output[0]
-    create_log(
-        json.dumps(
-            {"output": output, "text": data["data"], "id": str(random.getrandbits(8))}
-        )
+    model = pickle.load(
+        open(os.path.join(BASE_DIR, "emotion_classify/pickle_files/model_nb.pkl"), "rb")
     )
-    return jsonify({"output": str(output)})
+    app.config.from_pyfile(os.path.join(BASE_DIR, "emotion_classify/config/config.cfg"))
+    CORS(app)
+    mongo = PyMongo(app)
 
+    # Select the database
+    db = mongo.db
+    shared_components["db"] = db
+    return app
+
+
+def create_route(app):
+    """
+    Adds different rules to the urls
+    """
+    app.add_url_rule(rule="/", view_func=display_page, methods=["GET"])
+    app.add_url_rule(rule="/api/predict", view_func=predict, methods=["POST"])
+    app.add_url_rule(rule="/api/logs", view_func=fetch_logs, methods=["GET"])
+    app.add_url_rule(rule="/api/log/<log_id>", view_func=fetch_log, methods=["GET"])
+    app.add_url_rule("/api/log/<log_id>", view_func=update_log, methods=["UPDATE"])
+    app.add_url_rule("/api/log/<log_id>", view_func=remove_log, methods=["DELETE"])
+    return app
+
+
+app = init_app()
 
 if __name__ == "__main__":
     app.run("0.0.0.0", 3000, debug=True)  # Automatic run of script

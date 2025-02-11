e603361fc66483b667ce1fda7470c6452d8aeca1
Jasmin Karki <jasminkarki8@gmail.com>
2025-02-11 10:15:10 +0530

    Include checkpoint dir and ML constants

--- emotion_classify/config/config.py
+++ emotion_classify/config/config.py
@@ -2,10 +2,16 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+PACKAGE_DIR = os.path.join(BASE_DIR, "emotion_classify")
+
+CHECKPOINT_DIR = os.path.join(PACKAGE_DIR, "checkpoints")
+
 DATA_PATH = os.path.join(BASE_DIR, "data")
 
 RAW_DATAPATH = os.path.join(DATA_PATH, "raw")
 
 DATASET_NAME = "ISEAR.csv"
+RANDOM_STATE = 64
+TEST_SIZE = 0.2
 
 shared_components = {"db": None}
